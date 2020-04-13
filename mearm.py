8de6e67ef96459784b4e4c686ff187faeab656a1
Goeievent <59319807+Goeievent@users.noreply.github.com>
2020-04-13 18:06:25 +0200

    Create mearm.py

---
+++ mearm.py
@@ -0,0 +1,85 @@
+import time
+
+from mefunctions import ServoNul
+from mefunctions import ServoNul2
+from mefunctions import ServoPlus
+from mefunctions import ServoPlus2
+
+kit = ServoKit(channels = 16)
+
+while True:
+
+    index = 0
+
+    angle = 120
+
+    ServoNul(kit.servo[index], angle)
+
+
+    index = 1
+
+    angle = 140
+
+    ServoPlus(kit.servo[index], angle)
+
+
+    index = 2
+
+    angle = 130
+
+    ServoPlus(kit.servo[index], angle)
+
+
+    index = 3
+
+    angle = 150
+
+    ServoNul(kit.servo[index], angle)
+
+
+    index = 2
+
+    angle = 130
+
+    ServoPlus2(kit.servo[index], angle)
+
+
+    index = 1
+
+    angle = 140
+
+    ServoPlus2(kit.servo[index], angle)
+
+
+    index = 0
+
+    angle = 120
+
+    ServoNul2(kit.servo[index], angle)
+    
+    
+    index = 1
+
+    angle = 140
+
+    ServoPlus(kit.servo[index], angle)
+
+
+    index = 3
+
+    angle = 20
+
+    ServoNul2(kit.servo[index], angle)
+
+
+    index = 1
+
+    angle = 140
+
+    ServoPlus2(kit.servo[index], angle)
+
+    time.sleep(10)
+
+    
+    
+    
